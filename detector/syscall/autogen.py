fb3dbe0e1639b332ca1a6bc630fc9fb7c37a4ef4
PGHOON <park1578@purdue.edu>
2023-11-11 03:33:15 +0900

    enter syscall only

--- detector/syscall/autogen.py
+++ detector/syscall/autogen.py
@@ -12,6 +12,6 @@ def save_formatted_code_with_trace_syscall(input_file_path, output_file_path):
 TRACE_SYSCALL({syscall}, "{tp_name}")\n"""
             output_file.write(formatted_string)
 
-input_file_path = 'text.txt'
-output_file_path = 'SEC_Autogen.txt'
+input_file_path = 'enter_text.txt'
+output_file_path = 'SEC_enter_Autogen.txt'
 save_formatted_code_with_trace_syscall(input_file_path, output_file_path)
