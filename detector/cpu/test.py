f82bfed562ed1d3e8e1d30aa2d6b29b890466261
PGHOON <park1578@purdue.edu>
2023-11-10 21:40:48 +0900

    organize

--- detector/cpu/test.py
+++ detector/cpu/test.py
@@ -1,8 +1,6 @@
 from bcc import BPF
 from bcc.utils import printb
 
-# eBPF 프로그램
-# 각 CPU에서 실행 중인 태스크의 실행 시간을 모니터링합니다.
 prog = """
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
@@ -35,21 +33,17 @@ int do_count(struct pt_regs *ctx) {
 }
 """
 
-# BPF 프로그램 로드
 b = BPF(text=prog)
 b.attach_kprobe(event=b.get_syscall_fnname("schedule"), fn_name="do_count")
 
-# 이벤트 출력 형식
 def print_event(cpu, data, size):
     event = b["events"].event(data)
     printb(b"PID: %d COMM: %s CPU Time: %d ns" % (event.pid, event.comm, event.delta))
 
-# 이벤트에 콜백 함수 연결
 b["events"].open_perf_buffer(print_event)
 
 print("Monitoring CPU usage... Hit Ctrl-C to end.")
 
-# 무한 루프를 돌면서 이벤트 대기
 try:
     while True:
         b.perf_buffer_poll()
