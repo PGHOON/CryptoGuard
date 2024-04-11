a1b1ccd517f6e78906983581133dc25ff7dcbf6f
PGHOON <park1578@purdue.edu>
2024-04-11 19:22:40 +0900

    Feat: test4

--- CryptoGuard/CronGuard/cronrule.c
+++ CryptoGuard/CronGuard/cronrule.c
@@ -1,90 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-#define MAX_PATH_LENGTH 256
-#define MAX_LINE_LENGTH 1024
-
-int isWhitelisted(const char *path, const char *whitelist[], int whitelist_size) {
-    for (int i = 0; i < whitelist_size; ++i) {
-        if (strcmp(path, whitelist[i]) == 0) {
-            return 1;
-        }
-    }
-    return 0;
-}
+#define MAX_LINE_LENGTH 256
+#define ALLOWED_PATH "test.rules"
+#define TEMP_CRON_FILE "modified_cron"
 
 int main() {
-    FILE *fp_in, *fp_out;
+    char current_cron_file[] = "current_cron";
+
+    if (system("crontab -l > current_cron") == -1) {
+        perror("Error executing command");
+        return EXIT_FAILURE;
+    }
+
+    FILE *current_cron_fp = fopen(current_cron_file, "r");
+    if (current_cron_fp == NULL) {
+        perror("Error opening current_cron file");
+        return EXIT_FAILURE;
+    }
+
+    FILE *modified_cron_fp = fopen(TEMP_CRON_FILE, "w");
+    if (modified_cron_fp == NULL) {
+        perror("Error opening modified_cron file");
+        fclose(current_cron_fp);
+        return EXIT_FAILURE;
+    }
+
     char line[MAX_LINE_LENGTH];
-    char delete_command[MAX_PATH_LENGTH + 30]; 
-
-    printf("Reading crontab...\n");
-
-    fp_in = popen("crontab -l", "r");
-    if (fp_in == NULL) {
-        perror("Error opening crontab");
-        return EXIT_FAILURE;
-    }
-
-    int whitelist_size = 0;
-    printf("Current crontab:\n");
-    while (fgets(line, sizeof(line), fp_in) != NULL) {
-        printf("%s", line);
-        whitelist_size++;
-    }
-
-    fseek(fp_in, 0, SEEK_SET);
-
-    const char **whitelist = malloc((whitelist_size + 1) * sizeof(char *));
-    if (whitelist == NULL) {
-        perror("Error allocating memory for whitelist array");
-        pclose(fp_in);
-        return EXIT_FAILURE;
-    }
-
-    for (int i = 0; i < whitelist_size; ++i) {
-        if (fgets(line, sizeof(line), fp_in) != NULL) {
-            line[strcspn(line, "\n")] = '\0';
-            whitelist[i] = strdup(line);
+    while (fgets(line, sizeof(line), current_cron_fp)) {
+        if (strstr(line, ALLOWED_PATH) != NULL) {
+            fputs(line, modified_cron_fp);
         } else {
-            perror("Error reading crontab");
-            pclose(fp_in);
-            free(whitelist);
-            return EXIT_FAILURE;
-        }
-    }
-    whitelist[whitelist_size] = NULL; 
-
-    pclose(fp_in);
-
-    fp_out = fopen("modified_cron", "w");
-    if (fp_out == NULL) {
-        perror("Error creating modified_cron file");
-        free(whitelist);
-        return EXIT_FAILURE;
-    }
-
-    for (int i = 0; whitelist[i] != NULL; ++i) {
-        if (isWhitelisted(whitelist[i], whitelist, whitelist_size)) {
-            fputs(whitelist[i], fp_out);
-            fputs("\n", fp_out);
-        } else {
-            sprintf(delete_command, "crontab -l | sed '/%s/d' | crontab", whitelist[i]);
-            printf("Executing command: %s\n", delete_command);
-            system(delete_command);
-            printf("Crontab containing '%s' has been removed!\n", whitelist[i]);
+            printf("Removing cron: %s", line);
         }
     }
 
-    fclose(fp_out);
+    fclose(current_cron_fp);
+    fclose(modified_cron_fp);
 
-    for (int i = 0; i < whitelist_size; ++i) {
-        free((void *)whitelist[i]);
+    if (system("crontab modified_cron") == -1) {
+        perror("Error updating crontab");
+        return EXIT_FAILURE;
     }
-    free(whitelist);
 
-    remove("modified_cron");
+    if (unlink(current_cron_file) == -1 || unlink(TEMP_CRON_FILE) == -1) {
+        perror("Error deleting temporary files");
+        return EXIT_FAILURE;
+    }
+
+    printf("Crontab has been updated!\n");
 
     return EXIT_SUCCESS;
 }
